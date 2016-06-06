709226c5ad360289424a0c2cc4990758a9ac4ced
John Crooks <johnmcrooks@gmail.com>
2016-06-06 12:04:54 -0400

    Week 4 Day 1: Display User list

--- src/com/crooks/Main.java
+++ src/com/crooks/Main.java
@@ -4,15 +4,19 @@ import spark.ModelAndView;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
 
     static User user;
+    static ArrayList<User> userList = new ArrayList<User>();
+
 
     public static void main(String[] args) {
 	Spark.init();
-    Spark.get(          //Spark.get() at a minimum requires 2 arguments, The name of the route and the function to run
+
+        Spark.get(          //Spark.get() at a minimum requires 2 arguments, The name of the route and the function to run
             "/",                          //Name of the route
             (request, response)-> {       //This line will be the function that runs when the user lands on this;
                 HashMap m = new HashMap();
@@ -21,29 +25,33 @@ public class Main {
 
                 }else {
                     m.put("name", user.name);                 //Using user.name allows us to use the name the person enters rather than the hardcoded one from earlier
+                    m.put("users", userList);               //passing the User list through
                     return new ModelAndView(m, "home.html");  //this uses the mustacheTemplate Engine, and allows injection of content into the html using the mustache tag {{content}}
                 }
             },
             new MustacheTemplateEngine()  //To pull from a template a third argument is used, but now the lambda must  be altered
-    );
-    Spark.post(         //Post routes don't return html so will different from Get Routes; Posts do some work and redirect you to a get route.
+        );
+
+        Spark.post(         //Post routes don't return html so will different from Get Routes; Posts do some work and redirect you to a get route.
             "/login",                   //Name of the route
             ((request, response) -> {   //Function that runs...
                 String username = request.queryParams("username");  //this is how you call the information the user is passing to the server
                 user = new User(username);                          // Perform some action with the information
+                userList.add(user);                                 // Adds User to the User list
                 response.redirect("/");                             // redirect to "/"
                 return "";                                          // required to return something but since were redirecting a blank is sufficient
             }
-    ));
+        ));
 
-    Spark.post(
+
+        Spark.post(
             "/logout",
             (request, response) -> {
                 user = null;
                 response.redirect("/");
                 return "";
             }
-    );
+         );
 
 
 
